efe58bb1be101162579db6e9ed6dd7ecfd3175d0
web_dev <sukritprakash2020@gmail,com>
2024-12-25 03:57:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const grid = document.querySelector('.grid')
+const turnindicator = document.querySelector('.turn-indicator')
+const rstbtn = document.querySelector('.reset-btn')
+const board = Array.from({length:6},()=>Array(7).fill(0))
+
+let currentplayer =1;
+// red=1 and yellow =2
+
+// rendering the baord
+
+
+// to check a winner
+
+
+
+// Function to handle a move
+
+// Render the updated board
+
+ // Check for a winner
+
+ // Add click event listener to the grid
+// grid.addEventListener('click', handleMove);
+
+// Reset the game
